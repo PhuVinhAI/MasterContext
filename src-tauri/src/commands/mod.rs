44c16b3bb5b2ebc32ff559ecddd8207e4832379d
PhuVinhAI <vinhckid5@gmail.com>
2026-03-16 23:32:27 +0700

    Auto-commit: Add ai_commands module exports to commands/mod.rs

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,11 +1,17 @@
 // src-tauri/src/commands/mod.rs
-mod git_commands;
-mod group_commands;
-mod profile_commands;
-mod project_commands;
-mod settings_commands;
-mod utils;
-mod watcher_commands;
+pub mod ai_commands;
+pub mod git_commands;
+pub mod group_commands;
+pub mod profile_commands;
+pub mod project_commands;
+pub mod settings_commands;
+pub mod utils;
+pub mod watcher_commands;
 
 pub use ai_commands::*;
 pub use git_commands::*;
+pub use group_commands::*;
+pub use profile_commands::*;
+pub use project_commands::*;
+pub use settings_commands::*;
+pub use watcher_commands::*;
